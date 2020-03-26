b2f3d383bfa6d1c3e441f0206c7053c97169edb4
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-03-26 13:15:51 +0500

    Project Completed

--- src/App.js
+++ src/App.js
@@ -4,18 +4,31 @@ import Axios from "axios";
 import UserForm from "./components/UserForm";
 
 class App extends Component {
+  state = {
+    repos: null
+  };
   getUser = e => {
     e.preventDefault();
     const user = e.target.elements.username.value;
-    Axios.get(`https://api.github.com/users/${user}`).then(res => {
-      console.log(res);
-    });
+    if (user) {
+      Axios.get(`https://api.github.com/users/${user}`).then(res => {
+        if (res) {
+          const repos = res.data.public_repos;
+          this.setState({ repos });
+        }
+      });
+    }
   };
   render() {
     return (
       <div className="App">
         <h1>HTTP Calls in React</h1>
-        <UserForm getUser={this.getUser} />
+        <UserForm getUser={this.getUser} repos={this.state.repos} />
+        {this.state.repos ? (
+          <p>Number of repos : {this.state.repos}</p>
+        ) : (
+          <p>Please enter a username</p>
+        )}
       </div>
     );
   }
